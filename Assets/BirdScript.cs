f962496b48ccfa4025716de6ed9d57bf57c708a6
Shreya <shrey@udel.edu>
2025-02-28 20:34:33 -0500

    added stuff

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -2,31 +2,71 @@ using UnityEngine;
 
 public class BirdScript : MonoBehaviour
 {
+    public GameObject birdy;
     public Rigidbody2D myRigidbody;
     public float flapStrength;
     public LogicScript logic;
     public bool birdIsAlive = true;
+    public GameObject startScreen;
+    private bool gameStart = false;
+
+    // Added wings as a feature
+    public SpriteRenderer wings;
+    public Sprite up;
+    public Sprite down;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+        wings = transform.Find("Wing1").GetComponent<SpriteRenderer>();
+        myRigidbody.simulated = false;
     }
 
     // Update is called once per frame
     [System.Obsolete]
     void Update()
     {
+        if (!gameStart)
+        {
+            return;
+        }
+
         if(Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
         {
-            myRigidbody.velocity = Vector2.up * flapStrength;
+            birdy.GetComponent<Rigidbody2D>().linearVelocity = Vector2.up * 10;
+            // myRigidbody.velocity = Vector2.up * flapStrength;
+            wings.sprite = up;
         }
-    
+
+        if(Input.GetKeyUp(KeyCode.Space) && birdIsAlive)
+        {
+            wings.sprite = down;
+        }
+
+        if (myRigidbody.position.y > 17 && birdIsAlive)
+        {
+            logic.gameOver();
+            birdIsAlive = false;
+        }
+
+        else if (myRigidbody.position.y < -17 && birdIsAlive)
+        {
+            logic.gameOver();
+            birdIsAlive = false;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         logic.gameOver();
         birdIsAlive = false;
+    }
+
+    public void startedGame()
+    {
+        gameStart = true;
+        myRigidbody.simulated = true;
+        startScreen.SetActive(true);
     }
 }
--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -1,18 +1,38 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
+using System;
 
 public class LogicScript : MonoBehaviour
 {
     public int playerScore;
     public Text scoreText;
     public GameObject gameOverScreen;
+    public int highScore;
+    public Text result;
 
     [ContextMenu("Increase Score")]
     public void addScore(int scoreToAdd)
     {
-        playerScore = playerScore + scoreToAdd;
-        scoreText.text = playerScore.ToString();
+        if (gameOverScreen.activeSelf)
+        {
+            return;
+        }
+        else
+        {
+            playerScore = playerScore + scoreToAdd;
+            scoreText.text = playerScore.ToString();
+        }
+
+        // Using Player Prefs for Highest Score
+        if (playerScore > highScore)
+        {
+            highScore = playerScore;
+            PlayerPrefs.SetInt("highScore", highScore);
+            PlayerPrefs.Save();
+        }
+        updateHighest();
     }
 
     public void restartGame()
@@ -23,5 +43,18 @@ public class LogicScript : MonoBehaviour
     public void gameOver()
     {
         gameOverScreen.SetActive(true);
+    }
+
+    void Start()
+    {
+        highScore = PlayerPrefs.GetInt("highScore", 0);
+        updateHighest();
+    }
+
+    private void updateHighest()
+    {
+        if (highScore.ToString() != null){
+            result.text = "High Score: " + highScore.ToString();
+        }
     }
 }
